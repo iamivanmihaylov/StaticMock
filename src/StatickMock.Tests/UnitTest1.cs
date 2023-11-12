e07290180033558823a54e91aa77dd55f39b1950
Ivan Mihaylov <ivan4o03.it@gmail.com>
2023-11-12 12:32:50 +0200

    Inital creation of the projects

---
+++ src/StatickMock.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StatickMock.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/StatickMock/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StatickMock
+{
+    public class Class1
+    {
+
+    }
+}
